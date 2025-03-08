d4c7ec83967276051351806fc49f446c292a251d
luaenrique <lua.enriquezan@gmail.com>
2025-03-08 11:13:12 -0300

    feat: input embedding layer

---
+++ model.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbeddings(nn.Module):
+    
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+
+        # https://pytorch.org/docs/stable/generated/torch.nn.Embedding.html
+        # the idea here is to generate vectors of size d_model to represent words 
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+        
